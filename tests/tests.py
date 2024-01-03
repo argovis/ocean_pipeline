ec80ee8bf9ecdbfa96eebc4a354501677bbf19d0
billmills <you@example.com>
2024-01-03 18:59:45 -0500

    corrections

--- tests/tests.py
+++ tests/tests.py
@@ -6,8 +6,8 @@ def test_has_common_non_nan_value():
 	y = [None, 3, None]
 	z = [4, None, None]
 
-    assert helpers.test_has_common_non_nan_value(x,y)
-    assert not helpers.test_has_common_non_nan_value(x,z)
+	assert helpers.test_has_common_non_nan_value(x,y)
+	assert not helpers.test_has_common_non_nan_value(x,z)
 
 def test_find_bracket():
 
