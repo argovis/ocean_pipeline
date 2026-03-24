97f72ace7e083c9c50fbe37ae32d14f01a36862a
Katie Mills <you@example.com>
2026-03-24 16:08:25 -0400

    typo

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -395,7 +395,7 @@ def steric_hgt_anom(row, pressure_range):
         row['conservative_temperature'] = gsw.conversions.CT_from_t(row['absolute_salinity'], row['temperature'], row['pressure'])
     ## form an interpolated integration comb for the physical variables
     row['pressure'],row['conservative_temperature'], row['absolute_salinity'] = all_present(row['pressure'],row['conservative_temperature'], row['absolute_salinity'])
-    if numpy.shape((row['pressure'])) == ()
+    if numpy.shape((row['pressure'])) == ():
         return [None] # some deeply pathological cases will end up with only a single level at this point
     pressure_comb = integration_comb(pressure_range, spacing=0.2)
     SA_comb, _ = interpolate_to_levels(row, 'absolute_salinity', pressure_comb)
